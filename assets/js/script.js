fa7dfef50be3f2e4070a50d6639e1c1e9eba698a
TR94 <>
2023-05-30 10:34:30 +0000

    Add some function to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,50 @@ const choices = ["rock", "paper", "scissors", "lizard", "spock"];
  * Get button elements and add event listeners for each button
  */
 
-document.addEventListener("DOMContentLoaded", function()) {
+document.addEventListener("DOMContentLoaded", function(){
     for (let button of buttons) {
         if (playerScore || computerScore > 10) {
         button.addEventListener("click", runGame())
         } else  if (playerScore === 10) {
             alert("You win");
           } else (computerScore === 10) {
-            alert("Computer wins")
+            alert("Computer wins");
           }
     }
+})
+
+//The main game loop used for each players choice
+
+function (runGame) {
+
 }
+
+
+function (playerChoice) {
+
+}
+
+function (computerChoice) {
+    let computer = Math.floor(Math.random() *5);
+
+    switch (computer) {
+        case 0:
+            document.getElementById("computer-image").background = "url(/assets/images/Rock.png) no-repeat center center";
+            break;
+
+        case 1:
+            document.getElementById("computer-image").background = "url(/assets/images/Paper.png) no-repeat center center";
+            break;
+
+        case 2:
+            document.getElementById("computer-image").background = "url(/assets/images/Scissors.png) no-repeat center center";
+            break;
+
+        case 3:
+            document.getElementById("computer-image").background = "url(/assets/images/Lizard.png) no-repeat center center";
+            break;
+
+        case 4:
+        document.getElementById("computer-image").background = "url(/assets/images/Spock.png) no-repeat center center";
+        break;
+    }
