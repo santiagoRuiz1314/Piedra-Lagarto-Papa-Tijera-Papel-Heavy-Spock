638cc979631df7efe4ad8c7d984f810b153a5782
TR94 <>
2023-05-31 08:33:02 +0000

    add computerChoice JS function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,23 +7,23 @@ let lizardButton = document.getElementById("lizard");
 let spockButton = document.getElementById("spock");
 
 rockButton.addEventListener("click",function() {
-    playerImage('rock');
+    playerChoice('rock');
 })
 
 paperButton.addEventListener("click",function() {
-    playerImage('paper');
+    playerChoice('paper');
 })
 
 scissorsButton.addEventListener("click",function() {
-    playerImage('scissors');
+    playerChoice('scissors');
 })
 
 lizardButton.addEventListener("click",function() {
-    playerImage('lizard');
+    playerChoice('lizard');
 })
 
 spockButton.addEventListener("click",function() {
-    playerImage('spock');
+    playerChoice('spock');
 })
 
 /*
@@ -31,7 +31,7 @@ spockButton.addEventListener("click",function() {
 * on what is clicked
 */
 
-function playerImage(playType) {
+function playerChoice(playType) {
     switch (playType) {
         case "rock":
             document.getElementById('player-image').style.background="url(/assets/images/Rock.png)";
@@ -51,3 +51,37 @@ function playerImage(playType) {
     }
   }
 
+/* 
+* Random number generator makes computer choice and 
+* function feeds the correct image into the computer 
+* selection area of the game
+*/
+
+function computerChoice() {
+    
+    let ranNum = Math.floor(Math.random() * 4)
+   
+    switch (ranNum) {
+        case "0":
+            document.getElementById('computer-image').style.background="url(/assets/images/Rock.png)";
+            break;
+        case "1":
+            document.getElementById('computer-image').style.background="url(/assets/images/Paper.png)";
+            break;
+        case "2":
+            document.getElementById('computer-image').style.background="url(/assets/images/Scissors.png)";
+            break; 
+        case "3":
+            document.getElementById('computer-image').style.background="url(/assets/images/Lizard.png)";
+            break;
+        case "4":
+            document.getElementById('computer-image').style.background="url(/assets/images/Spock.png)";
+            break;
+    }
+}
+
+// main game function loop 
+
+function () {
+    
+}
